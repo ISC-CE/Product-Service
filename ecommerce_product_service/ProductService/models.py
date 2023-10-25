121529441bb80ee6ca316e1897ac326e5b880e46
rajeev-menon <rajeevmenonwky@gmail.com>
2023-10-24 19:39:49 -0500

    Product Table updtd

--- ecommerce_product_service/ProductService/models.py
+++ ecommerce_product_service/ProductService/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    #stock_quantity = models.PositiveIntegerField()
+    #image = models.ImageField(upload_to='products/', null=True, blank=True)
+
+    def __str__(self):
+        return self.name
